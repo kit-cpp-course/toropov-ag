6341f62e2a7077c3d52004b7457ed55c7684f45e
Aleksei Toropov <45897514+thetoropov@users.noreply.github.com>
2018-12-25 01:52:02 +0300

    first version

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "point.h"
+#include "vector.h"
+#include "triangulate.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	/**
+	* Инициализируем многоугольник
+	*/
+	point A;	//example
+	A.x = 0;
+	A.y = 0;
+
+	point B;
+	B.x = 4;
+	B.y = 0;
+
+	point C;
+	C.x = 2;
+	C.y = 3;
+
+	point D;
+	D.x = -1;
+	D.y = 0;
+
+	point E;
+	E.x = -2;
+	E.y = 3;
+
+	point F;
+	F.x = -2;
+	F.y = -3;
+
+	Vector <point> p;
+	p.push_back(A);
+	p.push_back(B);
+	p.push_back(C);
+	p.push_back(D);
+	p.push_back(E);
+	p.push_back(F);
+
+	const int count = 10;
+	point T[count][3];
+
+	/**
+	* Выполняем триангуляцию
+	*/
+	triangulate res;
+	res.triangulator(p, T);
+
+	/**
+	* Вывод решения триангуляции
+	*/
+	std::cout << "Количество треугольников в триангуляции: " << count_triang << std::endl;
+	//std::cout << "======" << std::endl;
+	for (int i = 0; i < count_triang; i++)
+	{
+		std::cout << "Трегольник " << i + 1 << std::endl;
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << T[i][j].x << " " << T[i][j].y << std::endl;
+		}
+	}
+
+	system("pause");
+
+}
---
+++ point.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <cmath>
+
+/**
+* Класс, описывающий точку
+*/
+class point
+{
+public:
+	point();
+	point(double x, double y);
+	~point();
+	/**
+	* Кординаты x, y
+	*/
+	double x, y;
+	/**
+	* Возвращает длину между точками
+	*
+	* @param point a, point b две точки
+	* @returns длина  отрезка между точками
+	*/
+	double distanceTo(point a, point b);
+};
+
+point::point()
+{
+	x = 0;
+	y = 0;
+}
+
+point::point(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+point::~point()
+{
+
+}
+
+double point::distanceTo(point a, point b)
+{
+	return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+}
